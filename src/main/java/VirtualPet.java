2d1c45f439dcf3c0fd82b63d4d900eceacd52ca3
DarkBomberX <EdwardPMills@gmail.com>
2020-01-19 18:40:08 -0500

    varibles created

--- src/main/java/VirtualPet.java
+++ src/main/java/VirtualPet.java
@@ -1,8 +1,43 @@
-
 public class VirtualPet {
+	
+	String name;
+	String description;
+	int hunger;
+	int thirst;
+	int bordom;
+	int sleep;
+	int bathroom;
 
-	public VirtualPet(String string, String string2, int i, int j, int k, int l) {
+	public VirtualPet(String name, String description, int hunger, int thirst, int bordom, int sleep, int bathroom) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public VirtualPet(String name, String description) {
+		
+	}
+	
+	public void feed() {
+		
+	}
+	
+	public void water() {
+		
+	}
+	
+	public void rest() {
+		
+	}
+
+	public void bathroom() {
+		
+	}
+	
+	public void play() {
+		
+	}
+	
+	public void tick() {
+		
 	}
 
 }
