08af513a4c2e7ecc77c2ddb54ea24e54024de072
DarkBomberX <EdwardPMills@gmail.com>
2020-01-19 18:31:37 -0500

    forgot to save

--- src/test/java/VirtualPetTest.java
+++ src/test/java/VirtualPetTest.java
@@ -5,8 +5,42 @@ import org.junit.Test;
 public class VirtualPetTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void feed() {
+		
+	}
+	
+	@Test 
+	public void water() {
+		
+	}
+	
+	@Test
+	public void rest() {
+		
 	}
 
+	@Test
+	public void bathroom() {
+		
+	}
+	
+	@Test
+	public void play() {
+		
+	}
+	
+	@Test
+	public void tick() {
+		
+	}
+	
+	@Test
+	public void nameDescription() {
+		
+	}
+	
+	@Test
+	public void stats() {
+		
+	}
 }
