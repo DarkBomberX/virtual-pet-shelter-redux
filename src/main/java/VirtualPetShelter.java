8f30f3549a98d5711bcaaa0bf3decbd30609d4cd
DarkBomberX <EdwardPMills@gmail.com>
2020-01-19 21:56:02 -0500

    the shelter test work for all my methods

--- src/main/java/VirtualPetShelter.java
+++ src/main/java/VirtualPetShelter.java
@@ -1,39 +1,49 @@
 import java.util.HashMap;
+
 import java.util.Map;
 
 public class VirtualPetShelter {
 
-	Map<String, VirtualPet> pets = new HashMap<String, VirtualPet>();
-	
+	private Map<String, VirtualPet> pets = new HashMap<String, VirtualPet>();
+
 	public void boardPet(VirtualPet petToBoard) {
 		pets.put(petToBoard.getName(), petToBoard);
 	}
-	
-	public void adoptPet() {
-		
+
+	public void adoptPet(String name) {
+		pets.remove(name);
 	}
-	
+
 	public void feedAllPet() {
-		
+		for (Map.Entry<String,VirtualPet> entry : pets.entrySet()) {
+	        VirtualPet pet = entry.getValue();
+	        pet.feed();
+	    }
 	}
-	
+
 	public void waterAllPet() {
-		
+		for (Map.Entry<String,VirtualPet> entry : pets.entrySet()) {
+	        VirtualPet pet = entry.getValue();
+	        pet.water();
+	    }
 	}
-	
-	public void playWithPet() {
-		
+
+	public void playWithPet(String name) {
+		showOnePet(name).play();
 	}
-	
+
 	public void tickAll() {
-		
+		for (Map.Entry<String,VirtualPet> entry : pets.entrySet()) {
+	        VirtualPet pet = entry.getValue();
+	        pet.tick();
+	    }
 	}
-	
+
 	public Map<String, VirtualPet> showAllPets() {
 		return pets;
 	}
 
 	public VirtualPet showOnePet(String name) {
-		return null;
+		return pets.get(name);
 	}
 }
--- src/test/java/VirtualPEtShelterTest.java
+++ src/test/java/VirtualPEtShelterTest.java
@@ -9,57 +9,77 @@ public class VirtualPetShelterTest {
 
 	VirtualPetShelter virtualPetShelterTestObject;
 	VirtualPet pet1;
+	VirtualPet pet2;
 
 	@Before
 	public void setUp() {
 		virtualPetShelterTestObject = new VirtualPetShelter();
 		pet1 = new VirtualPet("Majima", "Dumb");
-		
-		
+		pet2 = new VirtualPet("lil Kanye", "Based");
+
 	}
 
 	@Test
 	public void boardPet() {
 		virtualPetShelterTestObject.boardPet(pet1);
-		assertEquals(1,virtualPetShelterTestObject.pets.size());
-		assertEquals("Dumb",virtualPetShelterTestObject.pets.get("Majima").getDescription());
+		assertEquals(1, virtualPetShelterTestObject.showAllPets().size());
+		assertEquals("Dumb", virtualPetShelterTestObject.showOnePet("Majima").getDescription());
 	}
 
 	@Test
 	public void adoptPet() {
-		
+		virtualPetShelterTestObject.boardPet(pet1);
+		virtualPetShelterTestObject.adoptPet("Majima");
+		assertEquals(0, virtualPetShelterTestObject.showAllPets().size());
+		assertEquals(null, virtualPetShelterTestObject.showOnePet("Majima"));
 	}
-	
+
 	@Test
 	public void feedAllPets() {
-		
+		virtualPetShelterTestObject.boardPet(pet1);
+		virtualPetShelterTestObject.boardPet(pet2);
+		virtualPetShelterTestObject.feedAllPet();
+		assertEquals(10, virtualPetShelterTestObject.showOnePet("Majima").getHunger());
+		assertEquals(10, virtualPetShelterTestObject.showOnePet("lil Kanye").getHunger());
 	}
-	
+
 	@Test
 	public void waterAllPets() {
-		
+		virtualPetShelterTestObject.boardPet(pet1);
+		virtualPetShelterTestObject.boardPet(pet2);
+		virtualPetShelterTestObject.waterAllPet();
+		assertEquals(11, virtualPetShelterTestObject.showOnePet("Majima").getThirst());
+		assertEquals(11, virtualPetShelterTestObject.showOnePet("lil Kanye").getThirst());
 	}
-	
+
 	@Test
 	public void playWithPet() {
-		
+		virtualPetShelterTestObject.boardPet(pet1);
+		virtualPetShelterTestObject.playWithPet("Majima");
+		assertEquals(9, virtualPetShelterTestObject.showOnePet("Majima").getBordom());
 	}
-	
+
 	@Test
 	public void tickAll() {
-		
+		virtualPetShelterTestObject.boardPet(pet1);
+		virtualPetShelterTestObject.boardPet(pet2);
+		virtualPetShelterTestObject.tickAll();
+		assertEquals(4, virtualPetShelterTestObject.showOnePet("Majima").getHunger());
+		assertEquals(4, virtualPetShelterTestObject.showOnePet("lil Kanye").getHunger());
+		assertEquals(4, virtualPetShelterTestObject.showOnePet("Majima").getSleep());
+		assertEquals(4, virtualPetShelterTestObject.showOnePet("lil Kanye").getSleep());
 	}
-	
+
 	@Test
 	public void seeAllPets() {
 		assertEquals(0, virtualPetShelterTestObject.showAllPets().size());
 	}
-	
+
 	@Test
 	public void seeOnePet() {
 		virtualPetShelterTestObject.boardPet(pet1);
 		VirtualPet vp = virtualPetShelterTestObject.showOnePet("Majima");
-		
+
 		assertEquals("Dumb", vp.getDescription());
 		assertEquals(5, vp.getHunger());
 	}
